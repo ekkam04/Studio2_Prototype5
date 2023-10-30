449da56b3b64c680f5818b62552039fbd9ccae33
Abbas Khan <Andyad999@gmail.com>
2023-10-30 14:20:33 +0400

    scripts for ghost statemachine and patrol

---
+++ Assets/AbbasStuff/PatrolPath.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+    public List<Transform> patrolPoints = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
